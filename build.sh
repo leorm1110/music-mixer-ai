17e41bc4628d05cb841e11f65c6eda8e012ccd74
leorm1110 <leorm1110@users.noreply.github.com>
2025-07-23 19:17:56 +0200

    Chore: Add diagnostics to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-
-# Exit on error
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Install dependencies
+# --- Diagnostic Commands ---
+echo "PYTHON_VERSION: $PYTHON_VERSION"
+echo "Python version used by build process:"
+python --version
+
+# --- Installation ---
+pip install --upgrade pip
 pip install -r requirements.txt
